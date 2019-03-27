032fe3647f3acff983076dd28794fc59ef8886b5
norleaf <norleaf@gmail.com>
2019-03-27 05:41:23 +0100

    Start
    its a start

---
+++ ArtificialIntelligence/Mouse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtificialIntelligence
+{
+    public class Mouse
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void Click(Point pt)
+        {
+            Cursor.Position = pt;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, pt.X, pt.Y, 0, 0);
+        }
+
+    }
+}
---
+++ ArtificialIntelligence/ScreenShot.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtificialIntelligence
+{
+    public class ScreenShotHelper
+    {
+        public Bitmap ScreenShot()
+        {
+            var screenShot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                        Screen.PrimaryScreen.Bounds.Height,
+                                        PixelFormat.Format32bppArgb);
+
+            using (var g = Graphics.FromImage(screenShot))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+            }
+
+            return screenShot;
+        }
+
+        public Point? GetFirstPixel(Bitmap bitmap, Color color)
+        {
+            for (var y = 0; y < bitmap.Height; y++)
+            {
+                for (var x = 0; x < bitmap.Width; x++)
+                {
+                    if (bitmap.GetPixel(x, y).Equals(color))
+                    {
+                        return new Point(x, y);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public bool ClickOnFirstPixel(Color color)
+        {
+            var pt = GetFirstPixel(ScreenShot(), color);
+
+            if (pt.HasValue)
+            {
+                Mouse.Click(pt.Value);
+            }
+
+            // return whether found pixel and clicked it
+            return pt.HasValue;
+        }
+
+    }
+
+    
+}
